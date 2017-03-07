7a62d0ba0e8ed46356ece2db274a32ed3f766c69
Shaiju Janardhanan <shaijujanardhanan@gmail.com>
2017-03-08 00:21:18 +0530

    Added Bubble sort and Selection sort algorithms

---
+++ CSharpSortingAlgorithms/CSharpSortingAlgorithms/Algorithms.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSortingAlgorithms
+{
+    public static class Algorithms
+    {
+        public static int [] SelectionSort(int[] input)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                for(int j = i; j < input.Length; j++)
+                {
+                    if (input[i] > input[j])
+                        swap(ref input[i], ref input[j]);
+                }
+            }
+            return input;
+        }
+
+        public static int [] BubbleSort(int[] input)
+        {
+            for (int k = input.Length - 1, l = 0 ; k >= l; l++)
+            {
+                for (int i = k, j = i - 1; j >= 0; i--, j--)
+                {
+                    if (input[i] > input[j])
+                        swap(ref input[i], ref input[j]);
+                }
+
+            }
+            return input;
+        }
+
+        private static void swap(ref int input1, ref int input2)
+        {
+            int temp = input1;
+            input1 = input2;
+            input2 = temp;
+        }
+
+        public static void display(int [] input)
+        {
+           foreach(var x in input)
+                Console.WriteLine($"element at {Array.IndexOf(input, x)} = {x}");
+        }
+    }
+
+    
+}
---
+++ CSharpSortingAlgorithms/CSharpSortingAlgorithmsTests/UnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static CSharpSortingAlgorithms.Algorithms;
+using NUnit.Framework;
+namespace CSharpSortingAlgorithmsTests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [Test]
+        public void BubbleSort_WithProperData()
+        {
+            int[] input = { 2, 3, 9, 1, 10, 5, 2 };
+            int[] expectedResult = { 10, 9, 5, 3, 2, 2, 1 };
+
+            int[] result = BubbleSort(input);
+            Assert.IsTrue(result.SequenceEqual(expectedResult));
+        }
+        [Test]
+        public void SelectionSort_WithProperData()
+        {
+            int[] input = { 2, 3, 9, 1, 10, 5, 2 };
+            int[] expectedResult = { 1, 2, 2, 3, 5, 9, 10 };
+            int[] result = SelectionSort(input);
+            Assert.IsTrue(result.SequenceEqual(expectedResult));
+        }
+    }
+}
